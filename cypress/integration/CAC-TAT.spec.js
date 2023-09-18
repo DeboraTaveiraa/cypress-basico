6a47881b38c9557c216cd23984fb320a7d1e3220
Débora Taveira <deborataveirapvh@gmail.com>
2023-09-18 13:26:47 -0400

    created tests using the select field

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -103,4 +103,49 @@ describe('Central de Atendimentos ao Cliente TAT', () => {
 
     cy.get('.success').should('be.visible');
   });
+
+  it('seleciona um produto (YouTube) por seu texto', () => {
+    cy.get('#product')
+      .select('YouTube')
+      .should('have.value', 'youtube');
+  });
+
+  it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+    cy.get('#product')
+      .select('mentoria')
+      .should('have.value', 'mentoria')
+  });
+
+  it('seleciona um produto (Blog) por seu índice', () => {
+    cy.get('#product')
+      .select(1)
+      .should('have.value', 'blog')
+  });
+
+  it('Seleciona um produto aleatoriamente', () => {
+    cy.get('select option')
+      .as('options')
+      .its('length', { log: false }).then(n => {
+        cy.get('@options', { log: false }).then($options =>{
+          const randomOptionIndex = Cypress._.random(n - 1);
+          
+          if (randomOptionIndex !== 0) {
+            const randomOptionText = $options[randomOptionIndex].innerText;
+  
+            cy.get('select').select(randomOptionText)
+
+          }
+
+        })
+      })
+  })
+
+  it.only('Deve preencher o campo de telefone e validar a quantidade', () => {
+    cy.get('#phone')
+      .type('00000000000')
+      .invoke('val')
+      .its('length')
+      .should('be.gt', 0)
+      .should('eq', 11)
+  })
 });
